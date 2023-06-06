8bfbc72cc4bc1cfce34d07be331a893a7d8b4319
Lenka Shynkarova <lllee@post.cz>
2023-06-06 22:06:08 +0200

    starters

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="p-2 m-2">
+    
+ </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
